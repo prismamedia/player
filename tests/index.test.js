eed93fb1a300dd8b227c7dac03a4bd1ef78f0da6
Yoriiis <2563298+yoriiis@users.noreply.github.com>
2025-09-24 16:15:49 +0200

    Add unit test

---
+++ tests/index.test.js
@@ -0,0 +1,41 @@
+import PrismaPlayer from '@src/index'
+
+const getInstance = () => new PrismaPlayer(document.querySelector('#playerWrapper-1'))
+
+let prismaPlayer
+
+describe('PrismaPlayer', () => {
+	beforeEach(() => {
+		document.body.innerHTML = `
+			<div
+				id="playerWrapper-1"
+				data-ads-core='{
+					"playerVideoId": "k4TUcxu2wZBNIyyyl5p",
+					"playerPosition": "Leader",
+					"playerId": "x168nc"
+				}'
+			>
+				<div id="player-1"></div>
+			</div>
+		`
+
+		prismaPlayer = getInstance()
+	})
+
+	describe('constructor', () => {
+		it('should set default variables as properties', () => {
+			expect(prismaPlayer.playerWrapper).toStrictEqual(document.querySelector('#playerWrapper-1'))
+			expect(prismaPlayer.player).toStrictEqual({
+				adsCore: {
+					playerVideoId: 'k4TUcxu2wZBNIyyyl5p',
+					playerPosition: 'Leader',
+					playerId: 'x168nc'
+				},
+				instance: null,
+				reboundCount: 0,
+				adCallCounter: 0,
+				prerollPosition: 1
+			})
+		})
+	})
+})
